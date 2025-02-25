03a26e53899f07f00db18dfe4a42b610c27de83c
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-02-25 23:24:10 +0530

    Mail Extension completed

--- Mail Writer Extension/Script.js
+++ Mail Writer Extension/Script.js
@@ -1,1 +1,15 @@
 console.log("Email Writer ")
+
+const observer=new MutationObserver((mutations)=>{
+    for(const mutation of mutations ){
+        const addedNodes=Array.from(mutation.addedNodes);
+        const hasComposeElements=addedNodes.some(node=>
+            node.nodeType===Node.ELEMENT_NODE
+            (node.matches('.aDh','.btC','[role="dialog"]') || node.querySelector('.aDh','.btC','[role="dialog"]'))
+        );
+        if(hasComposeElements){
+            console.log("Compose Window Detected");
+            setTimeout(injectButton,500);
+        }
+    }
+})
